7283841e6a59039604679e8c99ebaa29c878087f
Raymond <raymondkxng17@gmail.com>
2024-01-05 22:28:27 +0200

    commit

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
- 
-
-
-let burger = document.querySelector(".burger")
+//  toggle navlinks
+ let burger = document.querySelector(".burger")
 let navLinks = document.querySelector(".navlinks")
 
 burger.addEventListener("click", () => {
     navLinks.classList.toggle("active")
 })
+
+
+
+
+const myText = new SplitType(".hero-text")
+
+gsap.to('.char', {
+    y: 100,
+    stagger: 0.05,
+    delay: 0.2,
+    duration: .1
+})
+
 
 
 
