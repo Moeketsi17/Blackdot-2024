42a4187934e4a49e340ed7334c06c8a80db55a13
Raymond <raymondkxng17@gmail.com>
2024-01-16 14:36:34 +0200

    commit

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
-// GSAP
-import gsap from 'gsap';
-let tl = gsap.timeline();
-
-tl.to(".hero p", {duration: 1, y: 40,})
 
 
-//  toggle navlinks
-let burger = document.querySelector(".burger")
-let navLinks = document.querySelector(".navlinks")
-
-burger.addEventListener("click", () => {
-    navLinks.classList.toggle("active")
-});
 
 
 
---
+++ index.js
@@ -0,0 +1,17 @@
+//  toggle navlinks
+let burger = document.querySelector(".burger")
+let navLinks = document.querySelector(".navlinks")
+
+burger.addEventListener("click", () => {
+    navLinks.classList.toggle("active")
+});
+
+
+
+
+
+
+
+
+
+
